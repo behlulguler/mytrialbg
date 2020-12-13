65cacc35297ed97597deef909b971e0616da1f84
Behlül Güler <behlulguler@Behlul-MacBook-Pro.local>
2020-12-14 00:02:44 +0300

    First commit

---
+++ R/hello.R
@@ -0,0 +1,30 @@
+#' @title My trial package
+#'
+#' @description R package trial for functionality
+#'
+#' @param x
+#'
+#' @return
+#' @export
+#'
+#' @examples
+hello <- function(x){
+  print("Hello world!")
+}
+
+
+
+
+#' Title
+#'
+#' @param r
+#' @param x
+#' @param y
+#'
+#' @return
+#' @export
+#'
+#' @examples
+printer <- function(r,x,y){
+  print(paste0("x = ", x))
+}
